74c8ef07a5c18c37e2680eb4f1527de270dc07c6
ASIM-SAMI <eng.asimsami@gmail.com>
2020-12-06 17:44:17 +0300

    verbal questions completed

--- solutions.js
+++ solutions.js
@@ -1,4 +1,14 @@
-/*
-Your soulutions go in this file.
-Do not forget to link this file in the index.html file so that the browser can load it
-*/
+// 1. What is the difference between a parameter and an argument?
+
+//  prameters: are the names after the funcation name,
+//  we can use them as inputs values inside the function.
+
+//  arguments: the values we pass it to the function  when we call it.
+
+//==================================
+
+// 2. Within a function, what is the difference between return and console.log?
+
+// return: give us the values when we call the function, after execute the codes in that function.
+
+// console.log: to print any values u want to showup in the console of the browser.
